1fa313e129cfd04c8a2ea2f1b21d23217ed2fe99
Nathan deGraaf <nathanddegraaf@gmail.com>
2020-01-16 23:12:00 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Visual Bits Corprate Website Starter Layout
+
+## Developed by Nathan degraaf JR Web Developer
+
+## Site languages
+  - Node.js
+  - express
+  - sass
+  - Jquery
+
+
---
+++ app.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+
+app.set('view engine', 'pug');
+
+app.use(express.static(path.join(__dirname + '/assets')));
+
+app.use(express.urlencoded({extended: false}));
+
+
+app.get('/', (req, res) => {
+  res.render('index', {
+  });
+});
+
+
+app.get('/:page', (req, res) => {
+  res.render(req.params.page, {
+  });
+});
+
+app.use((err, req, res, next) => {
+  return res.status(500).render('error');
+});
+app.use((err, req, res, next) => {
+  return res.status(404).render('error');
+});
+
+const PORT = process.env.PORT || 3000
+
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
